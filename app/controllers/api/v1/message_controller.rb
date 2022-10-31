32995b502f3eb0dae9a26f6eda66c4aa4eb0f90b
NatiDeme <demelashnatnael@gmail.com>
2022-10-31 14:14:13 +0300

    Add fix to linter errors

--- app/controllers/api/v1/message_controller.rb
+++ app/controllers/api/v1/message_controller.rb
@@ -1,11 +1,16 @@
-class Api::V1::MessageController < ApplicationController
-    def index
-        @message = Message.order("RANDOM()").first
-        respond_to do |format|
-        format.html # index.html.erb
-        format.xml  { render :xml => @message.message }
-        format.json { render :json => @message.message }
-        end
-    end
-end
+# frozen_string_literal: true
 
+module Api
+  module V1
+    class MessageController < ApplicationController
+      def index
+        @message = Message.order('RANDOM()').first
+        respond_to do |format|
+          format.html # index.html.erb
+          format.xml { render xml: @message.message }
+          format.json { render json: @message.message }
+        end
+      end
+    end
+  end
+end
